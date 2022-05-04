2f5a82ebcd9e733c8c249c9eb0af1956eafee7a6
natian <natian@qq.com>
2022-05-04 23:54:48 +0800

    first commit

---
+++ src/main/java/com/jjt/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.jjt.git;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
