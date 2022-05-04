dacc8af8b5783d5d36e4e7c0275eafa616be8236
natian <natian@qq.com>
2022-05-04 23:57:00 +0800

    second commit

--- src/main/java/com/jjt/git/GitTest.java
+++ src/main/java/com/jjt/git/GitTest.java
@@ -3,9 +3,7 @@ package com.jjt.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello git");
+        System.out.println("hello git2");
+
     }
 }
